5eb467f5f22a879c0c4e7f49c5df9fb6f659f062
Daniel César Ngunza <114775878+danielngunza@users.noreply.github.com>
2024-01-14 18:59:05 +0100

    d

---
+++ aula011/exercicio008.js
@@ -0,0 +1,7 @@
+var vel = 90.5
+console.log(`A velocidade do carro é de ${vel}km/h`)
+
+if(vel > 60){
+    console.log('Vc passou o limite, MULTADO!')
+}
+console.log('Use sempre o cinto de segurança')
---
+++ aula011/exercicio009.js
@@ -0,0 +1,6 @@
+var pais = 'EUA'
+if(pais == 'Angola'){
+    console.log('Puro Mangóle')
+}else{
+    console.log('Num é daqui kkk')
+}
